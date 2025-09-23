1af8d4583a8ae692659c133f8221bf1ab35eeb2f
ana-manea <ana_manea@hotmail.com>
2025-09-22 22:05:21 -0300

    Correrção JS - Visualizar Camisa pelo Botão

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,16 @@ function selecionarImgProduto(imgMiniaturaEscolhida){
 }
 
 function mudarPreco(botaoEscolhido){
+    const coresImagens = {
+        "btnModeloPreto": "../img/produto1.png",
+        "btnModeloAzul": "../img/produto2.png", 
+        "btnModeloVerde": "../img/produto3.png",
+        "btnModeloCinza": "../img/produto4.png",
+        "btnModeloRosa": "../img/produto5.png"
+    };
+    
+    document.getElementById("imgPrincipal").src = coresImagens[botaoEscolhido.id];
+
     if(botaoEscolhido.id == "btnModeloPreto"){
         document.getElementById('precoProduto').innerHTML = "R$ 56,90";
         document.getElementById('qtdDisponivel').innerHTML = "171 peças disponíveis";
@@ -23,6 +33,39 @@ function mudarPreco(botaoEscolhido){
         document.getElementById('precoProduto').innerHTML = "R$ 69,90";
         document.getElementById('qtdDisponivel').innerHTML = "319 peças disponíveis";
     }    
+}
+
+function selecionarCor(imgEscolhida) {
+    document.getElementById("imgPrincipal").src = imgEscolhida.src;
+    
+    const imagemParaDados = {
+        "../img/produto1.png": {
+            preco: "R$ 56,90",
+            quantidade: "171 peças disponíveis"
+        },
+        "../img/produto2.png": {
+            preco: "R$ 56,90", 
+            quantidade: "152 peças disponíveis"
+        },
+        "../img/produto3.png": {
+            preco: "R$ 49,90",
+            quantidade: "213 peças disponíveis"
+        },
+        "../img/produto4.png": {
+            preco: "R$ 69,90",
+            quantidade: "353 peças disponíveis"
+        },
+        "../img/produto5.png": {
+            preco: "R$ 69,90",
+            quantidade: "319 peças disponíveis"
+        }
+    };
+    
+    const dados = imagemParaDados[imgEscolhida.src];
+    if (dados) {
+        document.getElementById('precoProduto').innerHTML = dados.preco;
+        document.getElementById('qtdDisponivel').innerHTML = dados.quantidade;
+    }
 }
 
 function mudarDisponivel (botaoEscolhido){
