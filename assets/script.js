91ca2adc942f7bb42be1efcf5a0620bd80d3a7a4
gigui <giogoncalvespadua13@gmail.com>
2025-09-27 16:21:52 -0300

    Borda com JS, Ajuste no nav do style_padrão e no CSS do Login/Index

--- assets/script.js
+++ assets/script.js
@@ -16,23 +16,48 @@ function mudarPreco(botaoEscolhido){
     if(botaoEscolhido.id == "btnModeloPreto"){
         document.getElementById('precoProduto').innerHTML = "R$ 56,90";
         document.getElementById('qtdDisponivel').innerHTML = "171 peças disponíveis";
+        document.getElementById('btnModeloPreto').classList.add('selecionado');
+        document.getElementById('btnModeloAzul').classList.remove('selecionado');
+        document.getElementById('btnModeloVerde').classList.remove('selecionado');
+        document.getElementById('btnModeloCinza').classList.remove('selecionado');
+        document.getElementById('btnModeloRosa').classList.remove('selecionado');
     }
     else if(botaoEscolhido.id == "btnModeloAzul"){
         document.getElementById('precoProduto').innerHTML = "R$ 56,90";
         document.getElementById('qtdDisponivel').innerHTML = "152 peças disponíveis";
+        document.getElementById('btnModeloAzul').classList.add('selecionado');
+        document.getElementById('btnModeloVerde').classList.remove('selecionado');
+        document.getElementById('btnModeloCinza').classList.remove('selecionado');
+        document.getElementById('btnModeloRosa').classList.remove('selecionado');
+        document.getElementById('btnModeloPreto').classList.remove('selecionado');
     }
     else if(botaoEscolhido.id == "btnModeloVerde"){
         document.getElementById('precoProduto').innerHTML = "R$ 49,90";
         document.getElementById('qtdDisponivel').innerHTML = "213 peças disponíveis";
+        document.getElementById('btnModeloVerde').classList.add('selecionado');
+        document.getElementById('btnModeloCinza').classList.remove('selecionado');
+        document.getElementById('btnModeloRosa').classList.remove('selecionado');
+        document.getElementById('btnModeloPreto').classList.remove('selecionado');
+        document.getElementById('btnModeloAzul').classList.remove('selecionado');
     }
     else if(botaoEscolhido.id == "btnModeloCinza"){
         document.getElementById('precoProduto').innerHTML = "R$ 69,90";
         document.getElementById('qtdDisponivel').innerHTML = "353 peças disponíveis";
+        document.getElementById('btnModeloCinza').classList.add('selecionado');
+        document.getElementById('btnModeloRosa').classList.remove('selecionado');
+        document.getElementById('btnModeloPreto').classList.remove('selecionado');
+        document.getElementById('btnModeloAzul').classList.remove('selecionado');
+        document.getElementById('btnModeloVerde').classList.remove('selecionado');
     }
     else if(botaoEscolhido.id == "btnModeloRosa"){
         document.getElementById('precoProduto').innerHTML = "R$ 69,90";
         document.getElementById('qtdDisponivel').innerHTML = "319 peças disponíveis";
-    }    
+        document.getElementById('btnModeloRosa').classList.add('selecionado');
+        document.getElementById('btnModeloPreto').classList.remove('selecionado');
+        document.getElementById('btnModeloAzul').classList.remove('selecionado');
+        document.getElementById('btnModeloVerde').classList.remove('selecionado');
+        document.getElementById('btnModeloCinza').classList.remove('selecionado');
+    }      
 }
 
 function selecionarCor(imgEscolhida) {
@@ -69,18 +94,36 @@ function selecionarCor(imgEscolhida) {
 }
 
 function mudarTamanho (botaoEscolhido){
+
     if (botaoEscolhido.id == "btnTamanhoP"){
         document.getElementById('tamanhoSelecionado').innerHTML = "Tamanho Selecionado: P";
+        document.getElementById('btnTamanhoP').classList.add('selecionado');
+        document.getElementById('btnTamanhoM').classList.remove('selecionado');
+        document.getElementById('btnTamanhoG').classList.remove('selecionado');
+        document.getElementById('btnTamanhoGG').classList.remove('selecionado');
     }
     else if (botaoEscolhido.id == "btnTamanhoM"){
         document.getElementById('tamanhoSelecionado').innerHTML = "Tamanho Selecionado: M";
+        document.getElementById('btnTamanhoM').classList.add('selecionado');
+        document.getElementById('btnTamanhoG').classList.remove('selecionado');
+        document.getElementById('btnTamanhoGG').classList.remove('selecionado');
+        document.getElementById('btnTamanhoP').classList.remove('selecionado');
     }
     else if (botaoEscolhido.id == "btnTamanhoG"){
         document.getElementById('tamanhoSelecionado').innerHTML = "Tamanho Selecionado: G";
+        document.getElementById('btnTamanhoG').classList.add('selecionado');
+        document.getElementById('btnTamanhoGG').classList.remove('selecionado');
+        document.getElementById('btnTamanhoM').classList.remove('selecionado');
+        document.getElementById('btnTamanhoP').classList.remove('selecionado');
     }
     else if (botaoEscolhido.id == "btnTamanhoGG"){
         document.getElementById('tamanhoSelecionado').innerHTML = "Tamanho Selecionado: GG";
+        document.getElementById('btnTamanhoGG').classList.add('selecionado');
+        document.getElementById('btnTamanhoG').classList.remove('selecionado');
+        document.getElementById('btnTamanhoM').classList.remove('selecionado');
+        document.getElementById('btnTamanhoP').classList.remove('selecionado');
     }
+
 }
 
 function usarCupom(button) {
